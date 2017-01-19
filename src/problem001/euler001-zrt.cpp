25b29a18d5c094724151d2cd0e56981c0c8683c5
frasnian <frasnian@users.noreply.github.com>
2017-01-19 00:42:55 -0600

    Problem 1 - Chg to count up instead of down for zrt version

--- src/problem001/euler001-zrt.cpp
+++ src/problem001/euler001-zrt.cpp
@@ -10,26 +10,24 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 // </copyright>
 //
-const int term  = 2;     // counting backwards from limit-1 to 3, inclusive
-const int limit = 1000;  // same reason as proof-of-correctness version
+const int limit = 1000;
 
 // For the zero-runtime version, we use template meta-programming to count
-// backwards from the maximum number (limit) to our terminating explicit 
-// specialization ("<term">), at which point further recursion stops and
-// we're done.
+// up to our terminating explicit specialization ("<limit">), at which point 
+// further recursion stops and we're done.
 template<int i>
-struct PrevMultiple {
+struct NextMultiple {
     enum { sum = (((i % 3) == 0) ||  ((i % 5) == 0) ? i : 0) 
-               + (PrevMultiple<i - 1>::sum)
+               + (NextMultiple<i + 1>::sum)
          };
 };
 
 template<>
-struct PrevMultiple<term> {
+struct NextMultiple<limit> {
     enum { sum = 0 };
 };
 
-long lSum = PrevMultiple<limit-1>::sum;
+long lSum = NextMultiple<3>::sum;
 
 #ifdef BUILD_ZRT_MAIN
 #include <iostream>
